bc1f136529a138e80c0a93b60e64343b1f9bb9b5
jay-raut <jayraut@my.yorku.ca>
2022-02-19 12:24:21 -0500

    Created project files
    Created project files via Android Studio.

---
+++ app/src/main/java/com/example/final_project_eecs_1022/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.final_project_eecs_1022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
